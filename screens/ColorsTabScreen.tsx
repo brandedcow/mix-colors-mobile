49a3f491c71012e76445e8b871018f47f4091d42
Chris Kang <chris.v.kang@gmail.com>
2021-09-17 14:49:18 -0700

    colors tab to use flatlist

--- screens/ColorsTabScreen.tsx
+++ screens/ColorsTabScreen.tsx
@@ -2,46 +2,49 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import useTrayState from '../state/useTrayState';
 import mixColors from '../lib/mixColors'
 import Color from '../types/Color';
+import { FlatList, ListRenderItem } from 'react-native';
 
 export default function ColorsTabScreen() {
   const trayState = useTrayState()
   const trays = trayState.getAll()
 
+  const renderItem: ListRenderItem<Array<Color>> = ({ item }) => {
+    const backgroundColor = mixColors(...item)
+    return (
+      <View style={styles.card}>
+        <View style={{
+          height: 40,
+          width: 120,
+          backgroundColor: backgroundColor === null ? 'white' : `#${backgroundColor.hex}`
+        }} />
+        <Ionicons name="md-trash-outline" size={24} color="black" />
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      {trays.map((tray: Array<Color>, idx: Number) => {
-        const backgroundColor = mixColors(...tray)
-        return (
-          <View
-            key={`tray-${idx}`}
-            style={styles.card}
-          >
-            <View style={{
-              height: 40,
-              width: 120,
-              backgroundColor: backgroundColor === null ? 'white' : `#${backgroundColor.hex}`
-            }} />
-            <Ionicons name="md-trash-outline" size={24} color="black" />
-          </View>
-        )
-      })}
-    </View>
+    <FlatList
+      data={trays}
+      renderItem={renderItem}
+      keyExtractor={(_,idx) => `tray${idx}`}
+      style={styles.container}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: '10%',
   },
   card: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingVertical: 20,
+    paddingHorizontal: '10%',
   }
 });
